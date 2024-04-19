a35aa31cb0ca663687ed8f9de0ba18317e433810
DedFishy <boynegregg312@gmail.com>
2024-04-19 19:21:22 -0400

    yass

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,17 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
+    println!("Hello World!");
+    println!("I'm definitely not addicted to crack :)");
+    panic!("Whoops!");
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
